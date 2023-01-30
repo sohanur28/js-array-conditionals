36c37b8b297e32c31992cc22dcc4cbcbbb823791
sohanur28 <gsrahman28@gmail.com>
2023-01-30 23:40:08 +0600

    nested-conditions

---
+++ multi-stage-conditions.js
@@ -0,0 +1,17 @@
+var money = 60;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if (danishPrice < money){
+    console.log('Dan Dan danish khamu');
+}
+else if (butterBread < money){
+    console.log('butter bon khamu');
+}
+else if (toastBiscuit < money){
+    console.log('chubai chubai toast biscuit khamu');
+}
+else {
+    console.log('Khali cha shasther jonno valo')
+}
