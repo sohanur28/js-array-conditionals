172966358ed4e1087cee7d3709d120b7b368a81b
sohanur28 <gsrahman28@gmail.com>
2023-01-30 17:46:43 +0600

    push pop

---
+++ push-pop.js
@@ -0,0 +1,15 @@
+var numbers = [78, 45, 98, 45];
+// use push to add element to an array as the last element array
+// numbers.push(63);
+// console.log(numbers);
+var friends = ['balam', 'kalam', 'salam'];
+friends.push('gelam', 'pailam');
+// friends.push('pailam');
+// console.log(friends);
+
+// use pop to get last element/last element remove
+console.log(numbers);
+// numbers.pop();
+var element = numbers.pop();
+console.log(numbers);
+console.log(element);
